5e1e9f52cd1048c56e5640b0cc3e9969dded5a6a
João Victor <jvfernandes.contato@gmail.com>
2022-12-02 20:19:53 -0300

    feat creating contact list

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,6 +1,8 @@
-import { Container, Header, ListContainer, ListBody } from "./styles";
+import { Container, Header, ListContainer, Card } from "./styles";
 
 import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -18,9 +20,65 @@ export default function ContactsList() {
           </button>
         </header>
 
-        <ListBody>
-          ok
-        </ListBody>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>mateus@devacademy.com.br</span>
+            <span>(41) 9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>mateus@devacademy.com.br</span>
+            <span>(41) 9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>mateus@devacademy.com.br</span>
+            <span>(41) 9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
       </ListContainer>
     </Container>
   );
