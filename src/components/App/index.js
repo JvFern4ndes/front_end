e127d433a068583c7545e75ad8bf40aba62c9dde
João Victor <jvfernandes.contato@gmail.com>
2022-12-03 07:21:08 -0300

    feat: configuring the react router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,26 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
-import ContactsList from '../ContactsList';
+import Routes from '../../Routes';
 
 import { Container } from './styles';
 
 function App() {
   return (
+    <BrowserRouter>
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
 
       <Container>
         <Header />
-        <ContactsList />
+        <Routes />
       </Container>
     </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
