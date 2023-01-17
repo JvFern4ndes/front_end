e0b411f2b048181f08dfa6eb2d3f67689f9bd9e8
João Victor <jvfernandes.contato@gmail.com>
2023-01-17 17:14:56 -0300

    feat: pivoting the modal component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,6 +16,7 @@ import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
+import Modal from '../../components/Modal';
 
 import ContactsService from '../../services/ContactsService';
 
@@ -66,6 +67,16 @@ export default function Home() {
   return (
     <Container>
       <Loader isLoading={isLoading} />
+
+      <Modal
+        danger
+        title="Tem certeza que deseja remover o contato "
+        confirmLabel="Deletar"
+        onCancel={() => alert('cancelou')}
+        onConfirm={() => alert('confirmou')}
+      >
+        <h1>Olá</h1>
+      </Modal>
 
       {contacts.length > 0 && (
         <InputSearchContainer>
